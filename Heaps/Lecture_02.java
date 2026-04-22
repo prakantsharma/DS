b7d08c87a025e51bdf15c782e0edbd79b8864edf
prakantsharma <prakantsharma276@gmail.com>
2026-04-22 07:07:26 +0530

    pq for objects

---
+++ Heaps/Lecture_02.java
@@ -0,0 +1,40 @@
+/*in this lecture we 
+learnt pq for objects  */
+import java.util.*;
+public class Lecture_02
+{   static class Student implements Comparable<Student>
+    { String name;
+        int rank;
+
+    public Student(String name,int rank){
+        this.name=name;
+        this.rank=rank;
+
+    }
+    @Override
+    public int compareTo(Student s2)
+    {
+        return this.rank-s2.rank;
+
+    }
+
+}
+    public static void main(String[] args)
+    {
+        PriorityQueue<Student> pq=new PriorityQueue<>();
+        pq.add(new Student("A",4)); //(O(logn))
+        pq.add(new Student("B",1));
+        pq.add(new Student("C",3));
+        pq.add(new Student("D",4));
+        pq.add(new Student("E",10));
+        while(!pq.isEmpty())
+        {
+            System.out.println(pq.peek().name+"->"+pq.peek().rank);//(O(1))
+            pq.remove();//(O(logn))
+        }
+
+
+        
+    }
+
+}
